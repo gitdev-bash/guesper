15ea72d33328d27c989b82d188a77be73324f7ad
gitdev-bash <63194733+gitdev-bash@users.noreply.github.com>
2024-06-03 23:19:10 +0000

    Update guessper.py syntax and language

--- guessper.py
+++ guessper.py
@@ -1,41 +1,50 @@
-#!/bin/python3
-print("Please enter the maximum posible number.")
-#define generatecards
-def generatecards(maxnum, israndome):
-	import random
-	if maxnum.isdigit() == False:
-		print("ONLY NUMBERS!")
-		return()
-	cardlist = []
-	for card in range(len(bin(int(maxnum)).replace("0b", ""))):
-		cardlist.append([])
-	for num in range(1, int(maxnum)+1):
-		binnumrev=bin(int(num)).replace("0b", "")
-		binnum=binnumrev[::-1]
-		for x in range(len(binnum)):
-			if binnum[x] == "1":
-				cardlist[x].append(num)
-	if israndome == "True":
-		for sublist in range(len(cardlist)):
-			random.shuffle(cardlist[sublist])
-	return(cardlist)
+#!/usr/bin/env python3
 
-#define askq
+import random
+
+
+# define generatecards
+def generatecards(maxnum, isRandom):
+    if maxnum.isdigit() == False:
+        print("ONLY NUMBERS!")
+        return
+    cardlist = []
+    for card in range(len(bin(int(maxnum)).replace("0b", ""))):
+        cardlist.append([])
+    for num in range(1, int(maxnum) + 1):
+        binnumrev = bin(int(num)).replace("0b", "")
+        binnum = binnumrev[::-1]
+        for x in range(len(binnum)):
+            if binnum[x] == "1":
+                cardlist[x].append(num)
+    if isRandom:
+        for sublist in range(len(cardlist)):
+            random.shuffle(cardlist[sublist])
+    return cardlist
+
+
+# define askq
 def askq(cardlist):
-	cardnum=0
-	print("Enter the numbers infront of the lines if line has the number.")
-	for card in cardlist:
-		cardnum+=1
-		print(str(cardnum) + ". " + str(card))
-		print("")
-	return(input(":"))
+    cardnum = 0
+    print("Enter the numbers in front of the lines if line has the number.")
+    for card in cardlist:
+        cardnum += 1
+        print(str(cardnum) + ". " + str(card))
+        print("")
+    return input(":")
 
-#define calculatenum
-def calculatenum(idlistold):
-	idlistnew = idlistold.replace(" ", "").split(",")
-	num=0
-	for exp in idlistnew:
-		num+=2**(int(exp)-1)
-	return(str(num))
+
+# define calculatenum
+def calculatenum(idlist):
+    num = 0
+    for exp in idlist.replace(" ", "").split(","):
+        num += 2 ** (int(exp) - 1)
+    return num
+
+
+print("Please enter the maximum posible number.")
 while True:
-	print("Your number is " + calculatenum(askq(generatecards(input("max num?:"), "True"))))
+    print(
+        "Your number is "
+        + str(calculatenum(askq(generatecards(input("max num?:"), True))))
+    )
